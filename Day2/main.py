f22fb33e2d4e7cf089576dee24c57b45d207185c
Bertrand Dungan <bertrand.dungan@hireup.com.au>
2023-12-04 09:49:34 +1100

    Finish day 2

--- Day2/main.py
+++ Day2/main.py
@@ -1,6 +1,6 @@
 from functools import reduce
-import re
-from typing import Iterable, TypeAlias, TypedDict
+from re import search
+from typing import TypeAlias, TypedDict
 from pathlib import Path
 
 Hand = TypedDict("Hand", {"red": int, "green": int, "blue": int})
@@ -13,9 +13,9 @@ MAX_BLUE = 14
 
 
 def get_hand_details(hand: str) -> Hand:
-    red_search = re.search(r"([0-9]+) r", hand)
-    green_search = re.search(r"([0-9]+) g", hand)
-    blue_search = re.search(r"([0-9]+) b", hand)
+    red_search = search(r"([0-9]+) r", hand)
+    green_search = search(r"([0-9]+) g", hand)
+    blue_search = search(r"([0-9]+) b", hand)
 
     red = red_search.group(1) if red_search else 0
     green = green_search.group(1) if green_search else 0
@@ -25,7 +25,7 @@ def get_hand_details(hand: str) -> Hand:
 
 
 def get_game_details(index: int, input: str) -> Game:
-    game_result = re.search(r": (.*)", input)
+    game_result = search(r": (.*)", input)
     assert game_result is not None
     all_hands: str = game_result.group(1)
     individual_hands = all_hands.split(";")
@@ -33,12 +33,9 @@ def get_game_details(index: int, input: str) -> Game:
     return (index + 1, hand_details)
 
 
-def get_games(input: str) -> Iterable[Game]:
+def get_games(input: str) -> list[Game]:
     game_lines: list[str] = input.splitlines()
-    game_details = [
-        get_game_details(index, line) for index, line in enumerate(game_lines)
-    ]
-    return game_details
+    return [get_game_details(index, line) for index, line in enumerate(game_lines)]
 
 
 def hand_is_valid(hand: Hand) -> bool:
@@ -55,10 +52,26 @@ def is_game_valid(game: Game) -> bool:
     return all(hand_is_valid(hand) for hand in game[1])
 
 
+def get_min_cubes(game: Game) -> Hand:
+    hands = game[1]
+    red_cubes = max(hand["red"] for hand in hands)
+    green_cubes = max(hand["green"] for hand in hands)
+    blue_cubes = max(hand["blue"] for hand in hands)
+    return {"red": red_cubes, "green": green_cubes, "blue": blue_cubes}
+
+
+def get_hand_power(hand: Hand) -> int:
+    return hand["red"] * hand["green"] * hand["blue"]
+
+
 data_path = Path(__file__).with_name("prompt.txt")
 file_text = data_path.read_text()
 games = get_games(file_text)
 valid_game_ids = [game[0] for game in games if is_game_valid(game)]
-sum_of_ids = reduce(lambda acc, game_id: acc + game_id, valid_game_ids, 0)
+sum_of__valid_ids = reduce(lambda acc, game_id: acc + game_id, valid_game_ids, 0)
+print(f"Sum of valid ids {sum_of__valid_ids}")
 
-print(sum_of_ids)
+min_cubes_hands = [get_min_cubes(game) for game in games]
+powers_of_min_hands = [get_hand_power(hand) for hand in min_cubes_hands]
+sum_of_powers = sum(powers_of_min_hands)
+print(f"Sum of cube powers {sum_of_powers}")
