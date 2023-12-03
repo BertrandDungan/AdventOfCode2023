7aa924caeb5c52e4446bacebf517b075af76b497
Bertrand Dungan <bertrand.dungan@hireup.com.au>
2023-12-03 22:34:38 +1100

    Added full typing

--- Day2/main.py
+++ Day2/main.py
@@ -1,10 +1,10 @@
 from functools import reduce
 import re
-from typing import Iterable, TypeAlias
-from pyparsing import Path
+from typing import Iterable, TypeAlias, TypedDict
+from pathlib import Path
 
-
-Game: TypeAlias = tuple[int, dict[str, int]]
+Hand = TypedDict("Hand", {"red": int, "green": int, "blue": int})
+Game: TypeAlias = tuple[int, list[Hand]]
 
 
 MAX_RED = 12
@@ -14,6 +14,18 @@ MAX_BLUE = 14
 MAX_CUBES = MAX_RED + MAX_GREEN + MAX_BLUE
 
 
+def get_hand_details(hand: str) -> Hand:
+    red_search = re.search(r"([0-9]+) r", hand)
+    green_search = re.search(r"([0-9]+) g", hand)
+    blue_search = re.search(r"([0-9]+) b", hand)
+
+    red = red_search.group(1) if red_search else 0
+    green = green_search.group(1) if green_search else 0
+    blue = blue_search.group(1) if blue_search else 0
+
+    return {"red": int(red), "green": int(green), "blue": int(blue)}
+
+
 def get_game_details(input: str) -> Game:
     game_number = re.search(r"([0-9]+):", input)
     assert game_number is not None
@@ -21,7 +33,8 @@ def get_game_details(input: str) -> Game:
     assert game_result is not None
     all_hands: str = game_result.group(1)
     individual_hands = all_hands.split(";")
-    return  # todo
+    hand_details = map(get_hand_details, individual_hands)
+    return (int(game_number.group(1)), list(hand_details))
 
 
 def get_games(input: str) -> Iterable[Game]:
@@ -30,8 +43,20 @@ def get_games(input: str) -> Iterable[Game]:
     return game_details
 
 
-def is_game_valid(input: Game) -> bool:
+def hand_is_valid(hand: Hand) -> bool:
+    if hand["red"] > MAX_RED:
+        return False
+    if hand["green"] > MAX_RED:
+        return False
+    if hand["blue"] > MAX_BLUE:
+        return False
+    if (hand["red"] + hand["green"] + hand["blue"]) > MAX_CUBES:
+        return False
     return True
+
+
+def is_game_valid(game: Game) -> bool:
+    return all(hand_is_valid(hand) for hand in game[1])
 
 
 data_path = Path(__file__).with_name("test.txt")
