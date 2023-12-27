b7958cd7ad1ffa6dc624a5591cb3ff121bbeee8e
Bertrand <bertranddungan@gmail.com>
2023-12-27 12:54:06 +1000

    Clean up slightly

--- Day5/main.py
+++ Day5/main.py
@@ -10,9 +10,20 @@ class Mapping(NamedTuple):
     stop: int
 
 
-class SeedRange(NamedTuple):
+class AlmanacRange(NamedTuple):
     start: int
     stop: int
+
+
+MAPPING_REGEX = [
+    r"soil map:\n([0-9 \n]+)",
+    r"fertilizer map:\n([0-9 \n]+)",
+    r"water map:\n([0-9 \n]+)",
+    r"light map:\n([0-9 \n]+)",
+    r"temperature map:\n([0-9 \n]+)",
+    r"humidity map:\n([0-9 \n]+)",
+    r"location map:\n([0-9 \n]+)",
+]
 
 
 def find_numbers(text: str, pattern: Pattern) -> list[int]:
@@ -21,10 +32,10 @@ def find_numbers(text: str, pattern: Pattern) -> list[int]:
     return [int(result) for result in results.group(1).split()]
 
 
-def get_seeds(text: str) -> list[SeedRange]:
+def get_seeds(text: str) -> list[AlmanacRange]:
     all_seed_numbers = find_numbers(text, compile(r"seeds: ([0-9 ]+)\n"))
     seed_pairs = batched(all_seed_numbers, 2)
-    return [SeedRange(pair[0], pair[0] + pair[1]) for pair in seed_pairs]
+    return [AlmanacRange(pair[0], pair[0] + pair[1]) for pair in seed_pairs]
 
 
 def get_mapping(text: str, pattern: Pattern) -> list[Mapping]:
@@ -34,44 +45,46 @@ def get_mapping(text: str, pattern: Pattern) -> list[Mapping]:
     ]
 
 
-def matching_range(source: SeedRange, transformation: Mapping) -> bool:
+def matching_range(source: AlmanacRange, transformation: Mapping) -> bool:
     return source.start >= transformation.start and source.stop <= transformation.stop
 
 
-def overlapping_range(source: SeedRange, transformation: Mapping) -> bool:
+def overlapping_range(source: AlmanacRange, transformation: Mapping) -> bool:
     return source.start <= transformation.stop and transformation.start <= source.stop
 
 
-def transform(source: SeedRange, transformations: list[Mapping]) -> list[SeedRange]:
+def transform(
+    source: AlmanacRange, transformations: list[Mapping]
+) -> list[AlmanacRange]:
     for transformation in transformations:
         if matching_range(source, (matching_transformation := transformation)):
             return [
-                SeedRange(
+                AlmanacRange(
                     source.start + matching_transformation.transformation,
                     source.stop + matching_transformation.transformation,
                 )
             ]
         if overlapping_range(source, (matching_transformation := transformation)):
-            split_source = SeedRange(
+            split_source = AlmanacRange(
                 max(source.start, matching_transformation.start),
                 min(source.stop, matching_transformation.stop),
             )
 
             transformed_range = [
-                SeedRange(
+                AlmanacRange(
                     split_source.start + matching_transformation.transformation,
                     split_source.stop + matching_transformation.transformation,
                 )
             ]
 
-            first_half = SeedRange(source.start, split_source.start - 1)
+            first_half = AlmanacRange(source.start, split_source.start - 1)
             if (
                 first_half.start != split_source.start
                 and first_half.stop != split_source.stop
                 and first_half.start <= first_half.stop
             ):
                 transformed_range.extend(transform(first_half, transformations))
-            second_half = SeedRange(split_source.stop + 1, source.stop)
+            second_half = AlmanacRange(split_source.stop + 1, source.stop)
             if (
                 split_source.start != second_half.start
                 and second_half.stop != split_source.stop
@@ -83,38 +96,31 @@ def transform(source: SeedRange, transformations: list[Mapping]) -> list[SeedRan
     return [source]
 
 
+def almanac_transform(
+    input: list[AlmanacRange], mapping: list[Mapping]
+) -> list[AlmanacRange]:
+    return list(chain(*[transform(almanacNum, mapping) for almanacNum in input]))
+
+
 data_path = Path(__file__).with_name("prompt.txt")
 file_text = data_path.read_text()
 
-seeds = get_seeds(file_text)
-soil_map = get_mapping(file_text, compile(r"soil map:\n([0-9 \n]+)"))
-fertiliser_map = get_mapping(file_text, compile(r"fertilizer map:\n([0-9 \n]+)"))
-water_map = get_mapping(file_text, compile(r"water map:\n([0-9 \n]+)"))
-light_map = get_mapping(file_text, compile(r"light map:\n([0-9 \n]+)"))
-temperature_map = get_mapping(file_text, compile(r"temperature map:\n([0-9 \n]+)"))
-humidity_map = get_mapping(file_text, compile(r"humidity map:\n([0-9 \n]+)"))
-location_map = get_mapping(file_text, compile(r"location map:\n([0-9 \n]+)"))
+transformation_mappings = [
+    get_mapping(file_text, compile(regex)) for regex in MAPPING_REGEX
+]
 
-soil_for_seeds = list(chain(*[transform(seed, soil_map) for seed in seeds]))
-fertiliser_for_soil = list(
-    chain(*[transform(seed, fertiliser_map) for seed in soil_for_seeds])
+soil_for_seeds = almanac_transform(get_seeds(file_text), transformation_mappings[0])
+fertiliser_for_soil = almanac_transform(soil_for_seeds, transformation_mappings[1])
+water_for_fertiliser = almanac_transform(
+    fertiliser_for_soil, transformation_mappings[2]
 )
-water_for_fertiliser = list(
-    chain(*[transform(seed, water_map) for seed in fertiliser_for_soil])
+light_for_water = almanac_transform(water_for_fertiliser, transformation_mappings[3])
+temperature_for_light = almanac_transform(light_for_water, transformation_mappings[4])
+humidity_for_temperature = almanac_transform(
+    temperature_for_light, transformation_mappings[5]
 )
-light_for_water = list(
-    chain(*[transform(seed, light_map) for seed in water_for_fertiliser])
+location_for_humidity = almanac_transform(
+    humidity_for_temperature, transformation_mappings[6]
 )
-temperature_for_light = list(
-    chain(*[transform(seed, temperature_map) for seed in light_for_water])
-)
-humidity_for_temperature = list(
-    chain(*[transform(seed, humidity_map) for seed in temperature_for_light])
-)
-location_for_humidity = list(
-    chain(*[transform(seed, location_map) for seed in humidity_for_temperature])
-)
-
 
 print(min(location_for_humidity).start)
-# 71100042 too low
